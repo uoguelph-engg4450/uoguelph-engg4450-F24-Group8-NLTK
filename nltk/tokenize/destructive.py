8c2a6f607148d6da0b47dc55b953a3ec0ee25306
Steven Bird <stevenbird1@gmail.com>
2024-08-09 15:53:02 +0930

    Update copyright year

--- nltk/tokenize/destructive.py
+++ nltk/tokenize/destructive.py
@@ -1,6 +1,6 @@
 # Natural Language Toolkit: NLTK's very own tokenizer.
 #
-# Copyright (C) 2001-2023 NLTK Project
+# Copyright (C) 2001-2024 NLTK Project
 # Author: Liling Tan
 #         Tom Aarsen <> (modifications)
 # URL: <https://www.nltk.org>
--- nltk/translate/chrf_score.py
+++ nltk/translate/chrf_score.py
@@ -1,6 +1,6 @@
 # Natural Language Toolkit: ChrF score
 #
-# Copyright (C) 2001-2023 NLTK Project
+# Copyright (C) 2001-2024 NLTK Project
 # Authors: Maja Popovic
 # Contributors: Liling Tan, Aleš Tamchyna (Memsource)
 # URL: <https://www.nltk.org/>
